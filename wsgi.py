0a3f277bc926a14d905faa57344e9c2cbc2b6caa
Dakota Benjamin <dakota.benjamin@gmail.com>
2015-10-14 09:39:01 -0400

    Hotfix: make application in wsgi

--- wsgi.py
+++ wsgi.py
@@ -13,13 +13,13 @@ except IOError:
 # line, it's possible required libraries won't be in your searchable path
 #
 
-import maprequests
+from maprequests import app as application
 #
 # Below for testing only
 #
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
 
-    httpd = make_server('localhost', 8051, maprequests.app)
+    httpd = make_server('localhost', 8051, application)
     # Wait for a single request, serve it and quit.
     httpd.serve_forever()
